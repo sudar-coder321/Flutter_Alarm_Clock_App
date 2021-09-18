ee89d6024c92b4e45def2dacf097d0766daf271f
Sudarshan.M.S <60535124+sudar-coder321@users.noreply.github.com>
2021-09-18 19:31:06 +0530

    FinalOne

---
+++ lib/models/menu_info.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+
+enum MenuType { clock, alarm, timer, stopwatch }
+
+class MenuInfo extends ChangeNotifier {
+  MenuType menuType;
+  String title;
+  String imageSource;
+
+  MenuInfo(this.menuType, {required this.title, required this.imageSource});
+
+  updateMenu(MenuInfo menuInfo) {
+    this.menuType = menuInfo.menuType;
+    this.title = menuInfo.title;
+    this.imageSource = menuInfo.imageSource;
+    notifyListeners();
+  }
+}
